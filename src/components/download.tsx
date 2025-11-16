6d6982e79799928c186db807cdf4a171a2ef9c9c
xionng <leeso0717@naver.com>
2025-11-16 21:09:36 +0900

    Feat : 구글플레이 버튼 수정

--- src/components/download.tsx
+++ src/components/download.tsx
@@ -4,15 +4,12 @@ import styles from "./commonStyles.module.css";
 export default function Download() {
   const APP_STORE_URL =
     "https://apps.apple.com/kr/app/crewing-%EC%8A%B9%EB%AC%B4%EC%9B%90-%EC%8A%A4%EC%BC%80%EC%A4%84-%EC%95%B1/id6752578142";
-  const GOOGLE_PLAY_URL = "";
+  const GOOGLE_PLAY_URL = "https://play.google.com/store/apps/details?id=com.likelion.crewing";
   const INSTAGRAM_URL = "https://www.instagram.com/crewing_official/";
 
   return (
     <div className={styles.download_wrapper}>
       <Image src="/img/main-logo.png" alt="메인로고" width={251} height={155} />
-      <p className={styles.comingSoonText2}>
-        진에어, 제주항공, 티웨이항공은 추후 스케줄 업데이트 예정입니다.
-      </p>
 
       <div className={styles.download_btn_container}>
         <a
@@ -35,15 +32,15 @@ export default function Download() {
           <p className={styles.download_btn}>App Store</p>
         </a>
 
-        <div
-          style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "0.8rem" }}
+        <a
+          href={GOOGLE_PLAY_URL}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.downloadBtn}
         >
-          <div className={`${styles.downloadBtn} ${styles.downloadBtnDisabled}`}>
-            <Image src="/icon/ic-google.svg" alt="Google Play" width={26} height={27} />
-            <p className={styles.download_btn}>Google Play</p>
-          </div>
-          <p className={styles.comingSoonText}>Google Play 11월 5일 오픈 예정</p>
-        </div>
+          <Image src="/icon/ic-google.svg" alt="Google Play" width={26} height={27} />
+          <p className={styles.download_btn}>Google Play</p>
+        </a>
       </div>
     </div>
   );
