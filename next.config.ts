7c366d4a83908bc3e22ca1c1931a1780501d30f1
xionng <leeso0717@naver.com>
2026-03-05 20:10:27 +0900

    fix : ads.txt

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/:locale(ko|en)/app-ads.txt",
+        destination: "/app-ads.txt"
+      }
+    ];
+  }
+};
 
 export default withNextIntl(nextConfig);
